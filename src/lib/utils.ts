b9827ca784d1edaa076ceed93cb746f8985090b1
harryIsADev <harrymail1969@gmail.com>
2025-06-29 07:43:04 +0000

    remove unused imports

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { remark } from "remark";
-import html from "remark-html";
+// import html from "remark-html";
 import remarkParse from "remark-parse";
 
 export function cn(...inputs: ClassValue[]) {
